828f74dc6b62d085a461ca2776968b0356f04d57
arnanmaipradit <a_maidit@hotmail.com>
2012-10-10 13:34:57 +0700

    change Guiresult

--- src/Guiresult.java
+++ src/Guiresult.java
@@ -1,20 +1,28 @@
 import javax.swing.*;
+
 import java.awt.*;
+import java.util.Set;
 
 
 
 public class Guiresult {
 	public  JFrame frame ;
 	
-	
 	public Guiresult(){
 		
 		frame = new JFrame("Result");
+		
 	}
 	
 	public void show(int i , String name , int score){
 		
-		
+	for(int c = 0 ;c<i ;c++ ){
+		JLabel label  = new JLabel();
+		label.setText( c+1+"     "+name+"     "+score);
+	}
+		 
+	
+		 
 	}
 	public void setvisible(boolean i){
 		frame.setVisible(i);
