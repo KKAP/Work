2c36133d6bca3f683a983393806ca680c08c8832
arnanmaipradit <a_maidit@hotmail.com>
2012-10-10 11:16:55 +0700

    add Controlresult and Guiresult

--- src/Controlresult.java
+++ src/Controlresult.java
@@ -1,7 +1,25 @@
+import java.util.Iterator;
+import java.util.Set;
 
 public class Controlresult {
-	Guiresult guiresult;
-	public void du(Ballotbox ballotbox){
-		
+	Ballotbox ballotbox;
+
+	Controlresult(Ballotbox ballotbox) {
+		this.ballotbox = ballotbox;
+		Guiresult guiresult = new Guiresult();
+
+	}
+
+	public void show(Ballotbox ballotBox) {
+		GUIshowresult guiShow = new GUIshowresult();
+		Set<String> set = ballotBox.getBallotBox().keySet();
+		Iterator iter = (Iterator) set.iterator();
+		while (iter.hasNext()) {
+			String key = (String) iter.next();
+			int a = (Integer) ballotBox.getBallotBox().get(key);
+			guiShow.guiResult(key + "", a + "");
+			guiShow.setShow(true);
+
+		}
 	}
 }
--- src/Guiresult.java
+++ src/Guiresult.java
@@ -1,4 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+
 
 public class Guiresult {
-
+	public  JFrame frame ;
+	
+	
+	public Guiresult(){
+		
+		frame = new JFrame("Result");
+	}
+	
+	public void show(int i , String name , int score){
+		
+		
+	}
+	public void setvisible(boolean i){
+		frame.setVisible(i);
+		
+	}
 }
