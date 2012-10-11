2d08e54feac031dfd722a5f134f78f7b07d3c617
Krittin Jaratmanachot <es.blah@gmail.com>
2012-10-11 11:52:33 +0700

    เเก้ดอก นู่นๆ นี่ๆ

--- src/Ballotbox.java
+++ src/Ballotbox.java
@@ -7,19 +7,29 @@ import java.util.Map;
 
 public class Ballotbox {
 	private Map<String, Integer> map = new HashMap<String, Integer>();
-
-    /*return score of project*/
+/**
+ * 
+ * @param nameProject get is to use to check a Score in Map.
+ * @return a Score from a Map.
+ */
 	public int getScore(String nameProject) {
 		return map.get(nameProject);
 	}
-    /*add bollot */
+/**
+ * 
+ * @param a get this to use to be a key to add a ballot.
+ * this method is add Ballot method.
+ */
 	public void addBallot(String a) {
 		if (map.containsKey(a)) {
 			map.put(a, map.get(a) + 1);
 		} else
 			map.put(a, 1);
 	}
-    /*return Ballotbox*/
+/**
+ *  
+ * @return a Map of a all the voted.
+ */
 	public Map getBallotBox() {
 		return map;
 	}
