4d39631daaacff3e9b581a15669a4cd4b3e65151
Krittin Jaratmanachot <es.blah@gmail.com>
2012-10-11 11:41:00 +0700

    เพิ่มมม javaDoc สำหรับ class student

--- src/Student.java
+++ src/Student.java
@@ -5,13 +5,27 @@ public class Student implements Voter {
 	private int remaining=2;
 	private String name;
 
+	/**
+	 * 
+	 * @param name is the name of the student.
+	 * this method is a constructor of student class.
+	 */
 	public Student(String name) {
 		this.name = name;
 	}
-	
+	/**
+	 * return remaining.
+	 */
 	public int getRemainning(){ return remaining;}
+	/**
+	 * remaining minus one.
+	 */
 	public void subtraceRemainning(){ remaining--;}
 	
+	/**
+	 * 
+	 * @return a name of the student.
+	 */
 	public String getName() {
 		return name;
 	}
