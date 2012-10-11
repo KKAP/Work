9cb76addb4beb2954aab38378085489c1421776d
arnanmaipradit <a_maidit@hotmail.com>
2012-10-11 11:49:13 +0700

    add javadoc

--- src/Ballot.java
+++ src/Ballot.java
@@ -5,29 +5,31 @@
  * 
  */
 
+
+/*Constructor*/
 public class Ballot {
 	private Voter voter;
 	private Projectdescription projectdescription;
 	private Categories categories;
 	private Ballotbox ballotBox;
-
+/*Constructor*/
 	public Ballot(Voter voter, Projectdescription project, Categories categories,Ballotbox ballotbox) {
 		this.voter=voter;
 		projectdescription = project;
 		this.categories = categories;
 		this.ballotBox=ballotbox;
 	}
-
+/*vote a score to Ballotbox*/
 	public void vote() {
 		ballotBox.addBallot(projectdescription.getNameProject());
 	}
 
 
-	
+/*return categories*/	
 	public Categories getCategories(){
 		return categories;
 	}
-
+/*return projectdescription*/
 	public Projectdescription getProjectdescription() {
 		return projectdescription;
 	}
--- src/Ballotbox.java
+++ src/Ballotbox.java
@@ -8,17 +8,18 @@ import java.util.Map;
 public class Ballotbox {
 	private Map<String, Integer> map = new HashMap<String, Integer>();
 
+    /*return score of project*/
 	public int getScore(String nameProject) {
 		return map.get(nameProject);
 	}
-
+    /*add bollot */
 	public void addBallot(String a) {
 		if (map.containsKey(a)) {
 			map.put(a, map.get(a) + 1);
 		} else
 			map.put(a, 1);
 	}
-
+    /*return Ballotbox*/
 	public Map getBallotBox() {
 		return map;
 	}
